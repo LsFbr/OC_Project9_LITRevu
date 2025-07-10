5722e1e26b938764d2d2256466d2d1e964b42e65
LsFbr <louisfabre@hotmail.fr>
2025-07-10 12:14:34 +0200

    Add login and registration URL paths to the main URL configuration

--- litrevu/litrevu/urls.py
+++ litrevu/litrevu/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from web_app import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("login/", views.login),
+    path("register/", views.register),
 ]
