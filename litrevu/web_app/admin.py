23ba6d6b3234e1dd974ddbad17aeec3a6ebd55be
LsFbr <louisfabre@hotmail.fr>
2025-07-18 15:41:09 +0200

    feat(admin.py): Register User, Ticket, Review, and UserFollows models in the Django admin site

--- litrevu/web_app/admin.py
+++ litrevu/web_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from web_app.models import User,Ticket, Review, UserFollows
+
+admin.site.register(User)
+admin.site.register(Ticket)
+admin.site.register(Review)
+admin.site.register(UserFollows)
+
