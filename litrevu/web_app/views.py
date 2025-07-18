420302bdf4021f518d4bbaca191b9fc8eabe67c9
LsFbr <louisfabre@hotmail.fr>
2025-07-18 15:43:12 +0200

    feat(views.py): Implement user login and logout functionality with authentication handling and user feedback

--- litrevu/web_app/views.py
+++ litrevu/web_app/views.py
@@ -1,8 +1,30 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect
+from web_app.forms import LoginForm
 
 
-def login(request):
-    return render(request, "web_app/login.html")
+def logout_user(request):
+    logout(request)
+    return redirect("login")
+
+
+def login_view(request):
+    form = LoginForm()
+    message = ""
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            user = authenticate(
+                username=form.cleaned_data["username"],
+                password=form.cleaned_data["password"],
+            )
+            if user is not None:
+                login(request, user)
+                message = f'Bonjour, {user.username}! Vous êtes connecté.'
+            else:
+                message = 'Identifiants invalides.'
+    return render(request, "web_app/login.html", context={"form": form, "message": message})
 
 
 def register(request):
