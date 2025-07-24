61979ca27bdaea05f95d93e9f16093a7683b19f6
LsFbr <louisfabre@hotmail.fr>
2025-07-24 17:21:52 +0200

    feat(views.py): Enhance LoginView and add RegisterView for user registration with redirection to LOGIN_REDIRECT_URL

--- litrevu/web_app/views.py
+++ litrevu/web_app/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
+from django.conf import settings
 from django.shortcuts import redirect
 from django.views.generic import View
 from django.contrib.auth.mixins import LoginRequiredMixin
-from web_app.forms import LoginForm
+from web_app.forms import LoginForm, RegisterForm
 
 
 class LogoutView(View):
@@ -31,7 +32,7 @@ class LoginView(View):
             )
             if user is not None:
                 login(request, user)
-                return redirect('flux')
+                return redirect(settings.LOGIN_REDIRECT_URL)
             else:
                 message = 'Identifiants invalides.'
         return render(request, self.template_name, {"form": form, "message": message})
@@ -39,9 +40,21 @@ class LoginView(View):
 
 class RegisterView(View):
     template_name = "web_app/register.html"
+    form_class = RegisterForm
+    success_url = "login"
 
     def get(self, request):
-        return render(request, self.template_name)
+        form = self.form_class()
+        message = ""
+        return render(request, self.template_name, {"form": form, "message": message})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect(settings.LOGIN_REDIRECT_URL)
+        return render(request, self.template_name, {"form": form})
 
 
 class FluxView(LoginRequiredMixin, View):
